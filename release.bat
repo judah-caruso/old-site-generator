55b4865929f9a2a6ce1fb0a5863a0623f9cd11dd
Judah Caruso <judahrcr@gmail.com>
2021-05-27 02:50:46 -0500

    build script changes

--- release.bat
+++ release.bat
@@ -1,12 +1,10 @@
 @echo off
 
-echo Building site...
-
 call build_site.bat &&^
 echo judahcaruso.com > docs\CNAME &&^
 
-echo Pushing site... &&^
+echo *** Pushing site... &&^
 git add . && git commit -m "Site update: %time% on %date%" &&^
 git push origin main
 
-echo Site's been pushed: https://judah-caruso.github.io/
+echo *** Site's been pushed: https://judah-caruso.github.io/
